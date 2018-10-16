825bb40434e75951417f6f2d987c80233f33e535
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 23:47:59 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016232626) do
+ActiveRecord::Schema.define(version: 20181016234739) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20181016232626) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.boolean  "allow_create_artists"
+    t.boolean  "allow_create_songs"
   end
 
   create_table "songs", force: :cascade do |t|
