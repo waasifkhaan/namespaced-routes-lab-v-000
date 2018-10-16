eaf56d9047e1a77315411ba5fe912935074c487e
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 23:46:43 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -14,5 +14,5 @@ class Admin::PreferencesController < ApplicationController
   def preference_params
     params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
   end
-  
+
 end
