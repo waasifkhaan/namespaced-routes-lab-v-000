8cae258119ef05fd4ebff30a8114de03d208dfaa
Wasif Khan <waasif.khaan@gmail.com>
2018-10-16 23:59:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,22 @@
 class SongsController < ApplicationController
    before_action :set_preferences, only: [:index, :new]
-  def index
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @songs = @artist.songs
-      end
-    else
-      @songs = Song.all
-    end
-  end
+
+   def index
+     if params[:artist_id]
+       @artist = Artist.find_by(id: params[:artist_id])
+       if @artist.nil?
+         redirect_to artists_path, alert: "Artist not found"
+       elsif @preferences && @preferences.song_sort_order
+         @songs = @artist.songs.order(title: @preferences.song_sort_order)
+       else
+         @songs = @artist.songs
+       end
+     elsif @preferences && @preferences.song_sort_order
+       @songs = Song.order(title: @preferences.song_sort_order)
+     else
+       @songs = Song.all
+     end
+   end
 
   def show
     if params[:artist_id]
